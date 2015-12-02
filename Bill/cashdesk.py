2e9cfe966065eeace19272d98db0e471b27ddc73
petrunka <amo.dinamarca@abv.bg>
2015-12-02 20:07:34 +0200

    Updated Bill

--- Bill/cashdesk.py
+++ Bill/cashdesk.py
@@ -1,9 +1,9 @@
 class Bill:
-    def __init__(self, _amount):
-        self._amount = _amount
+    def __init__(self, amount):
+        self._amount = amount
 
     def __str__(self):
-        return "A {}$ bill".format(self._amount)
+        return "A {}$ bill".format(self.$amount)
 
     def __repr__(self):
         return self.__str__()
@@ -12,7 +12,7 @@ class Bill:
         return int(self._amount)
 
     def __eq__(self, other):
-        return self._amount == other._amount
+        return self._amount == other._get_amount()
 
     def __hash__(self):
         return int(self._amount)
@@ -21,17 +21,14 @@ class Bill:
         return self._amount
 
 class BatchBill:
-    def __init__(self, _bills):
-        self._bills = _bills
+    def __init__(self, bills):
+        self._bills = bills
 
     def __len__(self):
         return len(self._bills)
 
     def total(self):
-        amount = 0
-        for i in range(0, len(self._bills)):
-            amount+=self._bills[i].__int__()
-        return amount
+        return sum([bill.get_amount() for bill in self._bills])
 
 
     def __getitem__(self, index):
@@ -39,17 +36,13 @@ class BatchBill:
 
 class CashDesk:
     def __init__(self):
-        self._amount = 0
+        self.cash = []
 
     def take_money(self, amount):
-        if isinstance(amount, BatchBill):
-            self._amount+=amount.total()
-        elif isinstance(amount, Bill):
-            self._amount+=amount.__int__()
-            
+        self.cash.append(amount)            
 
     def total(self):
-        return self._amount
+        return sum([obj.total() for obj in self.cash])        
 
     def inspect(self):
         total = "We have a total of {}$ in the desk.".format(self._amount)
@@ -57,36 +50,3 @@ class CashDesk:
         for i in bills:
             total+="\n"+str(i)
         return total
-
-
-a = Bill(11)
-b = Bill(11)
-c = Bill(7)
-money_holder = {}
-money_holder[a]=1
-if b in money_holder:
-    money_holder[b]+=1
-print(money_holder)
-print(a)
-print(int(a))
-print(str(a))
-print(a==b)
-print(a==c)
-values = [2,5,10,20]
-bills = [Bill(value) for value in values]
-batch = BatchBill(bills)
-print(batch.total())
-print(batch.__len__())
-print(batch.__getitem__(1))
-for bill in batch:
-    print(bill)
-values = [10,20,50,100,100,100]
-bills = [Bill(value) for value in values]
-batch = BatchBill(bills)
-desk = CashDesk()
-desk.take_money(batch)
-print(desk.total())
-print(desk.inspect())
-desk.take_money(Bill(20))
-print(desk.total())
-    

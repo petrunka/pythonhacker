54b08c31577957c7d88ac411677335fd109b8669
petrunka <amo.dinamarca@abv.bg>
2016-01-19 22:11:27 +0200

    Update diveintopython.py

--- diveintopython.py
+++ diveintopython.py
@@ -1,5 +1,12 @@
 from math import sqrt
-from firstday import is_palindrome
+
+def palindrome(n):
+    r=n[:-1]
+    for i in range(0, (len(n)+1)//2):
+        if r[i]!=n[i]:
+            return False
+    return True
+
 def is_number_balanced(n):
     sum_a = 0
     sum_b = 0
@@ -30,7 +37,7 @@ def is_decreasing(n):
 def get_largest_palindrome(n):
     largest_palindrome = 0
     number = n
-    while is_palindrome(number) == False:
+    while palindrome(number) == False:
         number-=1
     return number
 
@@ -80,13 +87,3 @@ def sum_matrix(m):
         for j in i:
             amount+=j
     return amount
-
-
-print(is_number_balanced(10233102))
-print(is_increasing([1, 2, 4, 12, 27, 21]))
-print(is_decreasing([1, -2, -4, -12, -27, -121]))
-print(prime_numbers(100))
-print(get_largest_palindrome(463215))
-print(is_anagram('invinge','vininge3'))
-print(birthday_ranges([5, 10, 6, 7, 3, 4, 5, 11, 21, 300, 15], [(4, 9), (6, 7), (200, 225), (300, 365)]))
-print(sum_matrix([[1, 2, 3], [4, 5, 6], [7, 8, 9]]))
